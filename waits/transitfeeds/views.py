8a2ff72437ce7d69fc126e68f91e118fc9d664f5
Andy Chosak <andy@localstandout.com>
2011-11-20 14:46:08 -0500

    Minor bug fixes to wait time display.

--- waits/transitfeeds/views.py
+++ waits/transitfeeds/views.py
@@ -19,60 +19,23 @@ def waits(request, template_name):
 													destination_name__isnull=False,
 													line__isnull=False)
 		min_waits = predictions.values('location_name', 'line', 'destination_name').annotate(min_wait=Min('wait'))
-		waits_by_query[query.created] = min_waits
+		offset = (query.created - start_time).total_seconds()
+		waits_by_query[int(offset)] = min_waits
 
 	# Organize data by route, where a route is a combination of location, group, and line.
 	waits_by_route = {}
 	Route = namedtuple('Route', ['location_name', 'line', 'destination_name'])
-	for query_time, query_waits in waits_by_query.iteritems():
+	for offset, query_waits in waits_by_query.iteritems():
 		for route in query_waits:
 			index = Route(route['location_name'],
 						  route['line'],
 						  route['destination_name'])
 			if index not in waits_by_route:
-				waits_by_route[index] = {}
-			waits_by_route[index][query_time] = route['min_wait'] 
+				waits_by_route[index] = []
+			waits_by_route[index].append({ 'offset': offset, 'wait': route['min_wait'] });
 
 	waits_by_route = [(k,waits_by_route[k]) for k in sorted(waits_by_route.iterkeys())]
-	return render(request, template_name, { 'waits': waits_by_route })
-
-	predictions = RailPrediction.objects.filter(query__in=recent, destination_id__isnull=False, destination_name__isnull=False, line__isnull=False).order_by(
-		'location_name', 
-		'group', 
-		'line')
-
-	combinations = predictions.values('location_name', 'group', 'line').distinct()
-	
-	for combination in combinations:
-		location_name = combination['location_name']
-		group = combination['group']
-		line = combination['line']
-
-		combination_predictions = predictions.filter(location_name=location_name,
-													 group=group,
-													 line=line)
-		
-		if not combination_predictions:
-			continue
-	
-		destinations = combination_predictions.values('destination_name').distinct()
-		destinations = [destination['destination_name'] for destination in destinations]
-
-		wait_times = []
-		for query in recent:
-			min_wait = combination_predictions.filter(query=query).aggregate(Min('wait'))
-			min_wait = min_wait['wait__min']	
-		
-			if min_wait is not None:
-				wait_times.append(str(min_wait))
-			else:
-				wait_times.append('null')
-
-		wait = {'location_name': location_name,
-				'line': line,
-				'group': group,
-				'destinations': ', '.join(destinations),
-				'wait_times': ','.join(wait_times) }
-		waits.append(wait)
-	
-	return render(request, template_name, { 'waits': waits })
+	return render(request, template_name, { 
+		'waits': waits_by_route,
+		'duration': (end_time - start_time).total_seconds() 
+	})
